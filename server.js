265c1ec2f810f73ec7be78a600ac7d1ffbcd6af9
Not-Your-Man <154847073+Not-Your-Man@users.noreply.github.com>
2024-01-16 14:49:06 +0100

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(cors());
-app.use(bodyParser.json());
 
 const mongoURI = 'mongodb+srv://williamsmicheal237:%40Wrongman%21@cluster0.wmuc96c.mongodb.net/signup_db?retryWrites=true&w=majority';
 
@@ -22,23 +21,23 @@ const userSchema = new mongoose.Schema({
   password: String,
 });
 
-userSchema.pre('save', async function (next) {
-  try {
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(this.password, salt);
-    this.password = hashedPassword;
-    next();
-  } catch (error) {
-    next(error);
-  }
-});
-
 const User = mongoose.model('User', userSchema);
+
+app.use(bodyParser.json());
 
 app.post('/api/register', async (req, res) => {
   try {
     const { name, email, password } = req.body;
-    const user = new User({ name, email, password });
+
+    // Check if the email already exists in the database
+    const isEmailTaken = await User.exists({ email });
+    if (isEmailTaken) {
+      return res.status(400).json({ error: 'Email is already taken' });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = new User({ name, email, password: hashedPassword });
+
     await user.save();
     res.status(201).json({ message: 'User registered successfully' });
   } catch (error) {
@@ -51,11 +50,29 @@ app.post('/api/login', async (req, res) => {
   try {
     const { email, password } = req.body;
     const user = await User.findOne({ email });
-
+    
     if (user && (await bcrypt.compare(password, user.password))) {
       res.status(200).json({ message: 'Login successful' });
     } else {
       res.status(401).json({ error: 'Invalid credentials' });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: error.message || 'Internal server error' });
+  }
+});
+
+app.post('/api/change-password', async (req, res) => {
+  try {
+    const { email, newPassword } = req.body;
+    
+    const hashedPassword = await bcrypt.hash(newPassword, 10);
+    const result = await User.updateOne({ email }, { $set: { password: hashedPassword } });
+
+    if (result.nModified > 0) {
+      res.status(200).json({ message: 'Password changed successfully' });
+    } else {
+      res.status(404).json({ error: 'User not found or password unchanged' });
     }
   } catch (error) {
     console.error(error);
